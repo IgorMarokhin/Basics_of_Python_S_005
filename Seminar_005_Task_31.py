64f7b9e6b46076c91b92b9e6b4eecc256ad73416
IgorMarokhin <lawyer.mii222@gmail.com>
2023-03-24 20:35:19 +0300

    Добавлено дополнительное решение задачи 31 (решение в группе)

--- Seminar_005_Task_31.py
+++ Seminar_005_Task_31.py
@@ -19,3 +19,14 @@ for i in range(0, n1 + 1):
 print(*list_1)
 
 print(list_1[n1])
+
+num = int(input('Please input number: '))
+
+
+def fibonachy(n: int):
+    if n == 0 or n == 1:
+        return n
+    return fibonachy(n - 1) + fibonachy(n - 2)
+
+
+print(fibonachy(num))
